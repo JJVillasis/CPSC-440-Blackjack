b3f6cfe769526ffc4016c913a80c322a271ef961
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-04-19 16:06:24 -0700

    Added class Player

---
+++ Player.py
@@ -0,0 +1,26 @@
+import Deck
+
+class Player:
+
+    #Constructor of hand object
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    #Instance of object when using repr()
+    def __repr__(self):
+        return "Player: " + self.name + "; Cards in Hand = " + str(len(self.hand))
+
+    #Instance of object when using print()
+    def __str__(self):
+        return self.name
+
+    #Draw a card from the deck and add it to the hand
+    def draw(self, deck):
+        self.hand.append(deck.drawCard())
+        return self
+
+    #Print cards found in hand
+    def printHand(self):
+        for card in self.hand:
+            print(card)
